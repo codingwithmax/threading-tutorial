7292be72a9e9e4c3c5f6740edbb03a47965430bb
Max Schallwig <max@mobalyticshq.com>
2021-07-04 13:51:23 +0200

    Queues and master scheduler

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import time
 
+from multiprocessing import Queue
+
 from workers.WikiWorker import WikiWorker
-from workers.YahooFinanceWorkers import YahooFinacePriceWorker
+from workers.YahooFinanceWorkers import YahooFinancePriceScheduler
 
 
 def main():
+    symbol_queue = Queue()
     scraper_start_time = time.time()
 
     wikiWorker = WikiWorker()
-    current_workers = []
-    for symbol in wikiWorker.get_sp_500_companies():
-        yahooFinacePriceWorker = YahooFinacePriceWorker(symbol=symbol)
-        current_workers.append(yahooFinacePriceWorker)
+    yahoo_finance_price_scheduler_threads = []
+    num_yahoo_finance_price_workers = 4
+    for i in range(num_yahoo_finance_price_workers):
+        yahooFinancePriceScheduler = YahooFinancePriceScheduler(input_queue=symbol_queue)
+        yahoo_finance_price_scheduler_threads.append(yahooFinancePriceScheduler)
 
-    for i in range(len(current_workers)):
-        current_workers[i].join()
+    for symbol in wikiWorker.get_sp_500_companies():
+        symbol_queue.put(symbol)
+
+    for i in range(len(yahoo_finance_price_scheduler_threads)):
+        symbol_queue.put('DONE')
+
+    for i in range(len(yahoo_finance_price_scheduler_threads)):
+        yahoo_finance_price_scheduler_threads[i].join()
 
     print('Extracting time took:', round(time.time() - scraper_start_time, 1))
 
--- workers/YahooFinanceWorkers.py
+++ workers/YahooFinanceWorkers.py
@@ -5,19 +5,35 @@ import time
 import random
 
 
-class YahooFinacePriceWorker(threading.Thread):
-    def __init__(self, symbol, **kwargs):
-        super(YahooFinacePriceWorker, self).__init__(**kwargs)
-        self._symbol = symbol
-        base_url = 'https://finance.yahoo.com/quote/'
-        self._url = f'{base_url}{self._symbol}'
+class YahooFinancePriceScheduler(threading.Thread):
+    def __init__(self, input_queue, **kwargs):
+        super(YahooFinancePriceScheduler, self).__init__(**kwargs)
+        self._input_queue = input_queue
         self.start()
 
     def run(self):
-        time.sleep(30 * random.random())
+        while True:
+            val = self._input_queue.get()
+            if val == 'DONE':
+                break
+
+            yahooFinacePriceWorker = YahooFinacePriceWorker(symbol=val)
+            price = yahooFinacePriceWorker.get_price()
+            print(price)
+            time.sleep(random.random())
+
+
+class YahooFinacePriceWorker():
+    def __init__(self, symbol, **kwargs):
+        self._symbol = symbol
+        base_url = 'https://finance.yahoo.com/quote/'
+        self._url = f'{base_url}{self._symbol}'
+
+    def get_price(self):
         r = requests.get(self._url)
         if r.status_code != 200:
             return
         page_contents = html.fromstring(r.text)
-        price = float(page_contents.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')[0].text)
-        print(price)
+        raw_price = page_contents.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')[0].text
+        price = float(raw_price.replace(',', ''))
+        return price
