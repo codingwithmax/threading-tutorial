7f0053bdee573bc4b46c288e38f4f437d4ddb87c
Max Schallwig <max@codingwithmax.com>
2021-08-30 17:05:46 +0200

    16MultiprocessingPool

--- multiprocessing/main.py
+++ multiprocessing/main.py
@@ -1,41 +1,18 @@
-from multiprocessing import Process, Queue
-import time
+from multiprocessing import Pool, cpu_count
 
 
-def check_value_in_list(x, i, number_of_processes, queue):
-    max_number_to_check_to = 10**8
-    lower = int(i * max_number_to_check_to / number_of_processes)
-    upper = int((i + 1) * max_number_to_check_to / number_of_processes)
-    number_of_hits = 0
-    for i in range(lower, upper):
-        if i in x:
-            number_of_hits += 1
-
-    queue.put((lower, upper, number_of_hits))
+def square(x):
+    return x ** 2
 
 
 num_processes = 4
 comparison_list = [1, 2, 3]
-queue = Queue()
-start_time = time.time()
-processes = []
-for i in range(num_processes):
-    t = Process(target=check_value_in_list, args=(comparison_list, i, num_processes, queue))
-    processes.append(t)
 
-for t in processes:
-    t.start()
+num_cpu_to_use = max(1, cpu_count() - 1)
 
-for t in processes:
-    t.join()
+print('Number of cpus being used:', num_cpu_to_use)
 
-queue.put('DONE')
+with Pool(num_cpu_to_use) as mp_pool:
+    result = mp_pool.map(square, comparison_list)
 
-while True:
-    v = queue.get()
-    if v == 'DONE':
-        break
-    lower, upper, number_of_hits = v
-    print('Between', lower, 'and', upper, 'we have', number_of_hits,'values in the list')
-
-print('Everything took:', time.time() - start_time, 'seconds')
+print(result)
