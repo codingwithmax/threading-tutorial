bebb11c6ab73e42032b7ed09d0046071af234dba
Max Schallwig <max@codingwithmax.com>
2021-08-30 17:23:21 +0200

    17 Multiple arguments in multiprocessing pool map

--- multiprocessing/main.py
+++ multiprocessing/main.py
@@ -1,18 +1,22 @@
 from multiprocessing import Pool, cpu_count
+from functools import partial
 
 
-def square(x):
-    return x ** 2
+def square(y, addition_component, x):
+    return x ** y + addition_component
 
 
 num_processes = 4
 comparison_list = [1, 2, 3]
-
+power = 3
+addition_component = 2
 num_cpu_to_use = max(1, cpu_count() - 1)
 
 print('Number of cpus being used:', num_cpu_to_use)
 
+partial_function = partial(square, power, addition_component)
+
 with Pool(num_cpu_to_use) as mp_pool:
-    result = mp_pool.map(square, comparison_list)
+    result = mp_pool.map(partial_function, comparison_list)
 
 print(result)
