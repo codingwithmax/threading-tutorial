32a0bfcc8603748b04e380d3974f5cb11b770206
Max Schallwig <max@codingwithmax.com>
2021-09-01 14:31:45 +0200

    21AsyncTasks

--- async/main.py
+++ async/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 
 async def async_sleep():
@@ -7,12 +8,17 @@ async def async_sleep():
 
 
 async def return_hello():
-    return 'Hello'
+    print('Hello')
 
 
 async def main():
-    await async_sleep()
-    hello = await return_hello()
-    print(hello)
+    task = asyncio.create_task(async_sleep())
+    start = time.time()
+    await asyncio.sleep(5)
+    print('Finished sleeping for 5 seconds, took', time.time() - start)
+    await task
+    print('Finished awaiting task, took', time.time() - start)
+    await return_hello()
+    print('Finished printing hello, took', time.time() - start)
 
 asyncio.run(main())
