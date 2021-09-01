b5893f09295f65323bd5bddac8bdf23cc4bbaed9
Max Schallwig <max@codingwithmax.com>
2021-09-01 13:31:06 +0200

    18MultiprocessingMultipleVaryingArguments

--- multiprocessing/main.py
+++ multiprocessing/main.py
@@ -1,22 +1,23 @@
 from multiprocessing import Pool, cpu_count
-from functools import partial
 
 
-def square(y, addition_component, x):
-    return x ** y + addition_component
+def square(x, y):
+    return x ** y
 
 
 num_processes = 4
 comparison_list = [1, 2, 3]
-power = 3
-addition_component = 2
+power_list = [4, 5, 6]
+
 num_cpu_to_use = max(1, cpu_count() - 1)
 
 print('Number of cpus being used:', num_cpu_to_use)
 
-partial_function = partial(square, power, addition_component)
-
+prepared_list = []
+for i in range(len(comparison_list)):
+    prepared_list.append((comparison_list[i], power_list[i]))
+print('List to use as input:', prepared_list)
 with Pool(num_cpu_to_use) as mp_pool:
-    result = mp_pool.map(partial_function, comparison_list)
+    result = mp_pool.starmap(square, prepared_list)  # [(1, 4), (2 ,5), (3, 6)]
 
 print(result)
