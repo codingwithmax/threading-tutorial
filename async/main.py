3ebd0d3be86196db909d135f80782d69e24483b7
Max Schallwig <max@codingwithmax.com>
2021-09-02 13:46:39 +0200

    Update main.py

--- async/main.py
+++ async/main.py
@@ -2,21 +2,21 @@ import asyncio
 import time
 
 
-async def async_sleep():
-    print('in async sleep, at time', time.time())
-    await asyncio.sleep(5)
-    print('Finished sleeping')
+async def async_sleep(n):
+    print('Before sleep', n)
+    await asyncio.sleep(n)
+    print('After sleep', n)
 
 
 async def print_hello():
-    print('in print hello, at time', time.time())
     print('Hello')
 
 
 async def main():
-
     start = time.time()
-    await asyncio.gather(async_sleep(), print_hello(), async_sleep())
-    print('Finished gather, took', time.time() - start)
+    await asyncio.gather(async_sleep(2), async_sleep(1), print_hello())
+    print('total time:', time.time() - start)
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    asyncio.run(main())
