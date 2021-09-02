78e2fbbf2bb24c0b120982a1e82b7f75221736a0
Max Schallwig <max@codingwithmax.com>
2021-09-02 13:59:36 +0200

    24AsyncFor

--- async/main.py
+++ async/main.py
@@ -2,10 +2,20 @@ import asyncio
 import time
 
 
+<<<<<<< Updated upstream
 async def async_sleep():
     print('in async sleep, at time', time.time())
     await asyncio.sleep(5)
     print('Finished sleeping')
+=======
+async def async_sleep(n):
+    print('Before sleep', n)
+    n = max(2, n)
+    for i in range(1, n):
+        yield i
+        await asyncio.sleep(i)
+    print('After sleep', n)
+>>>>>>> Stashed changes
 
 
 async def print_hello():
@@ -16,6 +26,7 @@ async def print_hello():
 async def main():
 
     start = time.time()
+<<<<<<< Updated upstream
     try:
         await asyncio.gather(asyncio.wait_for(async_sleep(), 1),
                              print_hello(),
@@ -23,5 +34,11 @@ async def main():
     except asyncio.TimeoutError:
         print('Encountered timeout error')
     print('Finished gather, took', time.time() - start)
+=======
+    async for k in async_sleep(5):
+        print(k)
+    print('total time:', time.time() - start)
+
+>>>>>>> Stashed changes
 
 asyncio.run(main())
