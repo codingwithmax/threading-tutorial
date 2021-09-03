e0ac951e3c2976f418458c81fb1dcd83d45568a1
Max Schallwig <max@codingwithmax.com>
2021-09-03 17:07:30 +0200

    26AsyncWait

--- async/main.py
+++ async/main.py
@@ -1,26 +1,26 @@
 import asyncio
-import time
 
 
-async def async_sleep(n):
-    print('Before sleep', n)
-    n = max(2, n)
-    for i in range(1, n):
-        yield i
-        await asyncio.sleep(i)
-    print('After sleep', n)
-
-
-async def print_hello():
-    print('Hello')
+async def async_sleep(duration):
+    await asyncio.sleep(duration)
+    return duration
 
 
 async def main():
-    start = time.time()
-    async for k in async_sleep(5):
-        print(k)
-    print('total time:', time.time() - start)
+    pending = set()
+    for i in range(1, 11):
+        pending.add(asyncio.create_task(async_sleep(i)))
+
+    add_task = True
+
+    while len(pending) > 0:
+        done, pending = await asyncio.wait(pending, return_when='FIRST_COMPLETED')
+        for done_task in done:
+            print(await done_task)
+        if add_task:
+            pending.add(asyncio.create_task(async_sleep(1)))
+            add_task = False
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
